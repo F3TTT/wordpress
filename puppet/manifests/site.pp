9c380c4b940769ab8f782149d7b2e9b5f63eb1fe
F3TTT <ddoughty@epiqsystems.com>
2015-03-04 15:47:13 -0600

    correctly invoke include mysql

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,5 +1,6 @@
 node "wordpress.heartofamericait.com" { 
   include apache
+  include '::mysql::server'
 
   class { 'wordpress':
     install_url => 'http://www.wordpress.org/wordpress-3.8.tar.gz',
